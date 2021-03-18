bde61f83293ec665d2a84e483289adf41ed6ecc3
jperez-89 <jrwc1989@gmail.com>
2021-03-18 07:18:09 -0600

    Actualizando cards

--- dashboard.php
+++ dashboard.php
@@ -23,8 +23,8 @@ include_once("includes/bodyHead.inc");
                     <div class="card-body">
                          <div class="row no-gutters align-items-center">
                               <div class="col mr-2">
-                                   <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                                   <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                   <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Ingresos (Mensual)</div>
+                                   <div class="h5 mb-0 font-weight-bold text-gray-800">40,000</div>
                               </div>
                               <div class="col-auto">
                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -40,8 +40,8 @@ include_once("includes/bodyHead.inc");
                     <div class="card-body">
                          <div class="row no-gutters align-items-center">
                               <div class="col mr-2">
-                                   <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                                   <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                   <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Salidas (Anual)</div>
+                                   <div class="h5 mb-0 font-weight-bold text-gray-800">215,000</div>
                               </div>
                               <div class="col-auto">
                                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -57,7 +57,7 @@ include_once("includes/bodyHead.inc");
                     <div class="card-body">
                          <div class="row no-gutters align-items-center">
                               <div class="col mr-2">
-                                   <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+                                   <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Maquinaria Disponible</div>
                                    <div class="row no-gutters align-items-center">
                                         <div class="col-auto">
                                              <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
@@ -83,7 +83,7 @@ include_once("includes/bodyHead.inc");
                     <div class="card-body">
                          <div class="row no-gutters align-items-center">
                               <div class="col mr-2">
-                                   <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
+                                   <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Devoluciones</div>
                                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                               </div>
                               <div class="col-auto">
